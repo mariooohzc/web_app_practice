ec2a0282054dc61d8aa0e054b00fac1d77681dab
mariooohzc <mariohanzel@outlook.com>
2024-07-21 02:31:02 +0800

    changed made

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ app = FastAPI()
 
 @app.get("/")
 async def index():
-    return {"message": "Hello World"}
+    return {
+        "message": "changed made, should be something different  ajdf;akljdfl;ajl;fja;jdf;a"
+    }
