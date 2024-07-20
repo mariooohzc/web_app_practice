9531cb2c6ec910333af9e45b34e4b2e60f84887b
mariooohzc <mariohanzel@outlook.com>
2024-07-21 02:38:03 +0800

    add python cron

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 
+import aiocron
+import requests
+
+
+@aiocron.crontab("*/5 * * * *")
+async def self_ping():
+    response = requests.get("your_fastapi_endpoint")
+    print(f"Health check response: {response.status_code}")
+
+
 app = FastAPI()
 
 
