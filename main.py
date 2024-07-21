342cbf3ccedeb9055362a8d2d95d830e0704aa3d
mariooohzc <mariohanzel@outlook.com>
2024-07-21 12:35:33 +0800

    using httpx

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 import aiocron
-import requests
-import asyncio
+import httpx
 
 
 app = FastAPI()
@@ -9,10 +8,11 @@ app = FastAPI()
 
 @aiocron.crontab("*/1 * * * *")
 async def self_ping():
-    print("before sending request")
-    response = requests.get("https://web-app-practice.onrender.com")
-    print("response testing:", response)
-    print(f"Health check response: {response.status_code}, testing")
+    async with httpx.AsyncClient() as client:
+        print("before sending request")
+        response = await client.get("https://web-app-practice.onrender.com")
+        print("response testing:", response)
+        print(f"Health check response: {response.status_code}, testing")
 
 
 @app.get("/")
@@ -20,6 +20,3 @@ async def index():
     return {
         "message": "checking if packages are installed and whether poetry installation is working"
     }
-
-
-asyncio.get_event_loop().run_forever()
