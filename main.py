84608c1dfdfcda592de73eb3ef6be5c197539c6f
mariooohzc <mariohanzel@outlook.com>
2024-07-21 11:40:58 +0800

    remove cron

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import requests
 app = FastAPI()
 
 
-@aiocron.crontab("*/1 * * * *")
-async def self_ping():
-    print("before sending request")
-    response = requests.get("https://web-app-practice.onrender.com")
-    print("response testing:", response)
-    print(f"Health check response: {response.status_code}, testing")
+# @aiocron.crontab("*/2 * * * *")
+# async def self_ping():
+#     print("before sending request")
+#     response = requests.get("https://web-app-practice.onrender.com")
+#     print("response testing:", response)
+#     print(f"Health check response: {response.status_code}, testing")
 
 
 @app.get("/")
